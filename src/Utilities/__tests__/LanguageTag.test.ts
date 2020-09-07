a98a1d1a4827def28add86a1a69c66c449f00f79
Andrew Dawson <dawso.andrew@gmail.com>
2020-09-07 12:16:57 -0400

    Adds more tests for the language tag parser and removes the outdated test script.

--- src/Utilities/__tests__/LanguageTag.test.ts
+++ src/Utilities/__tests__/LanguageTag.test.ts
@@ -1,6 +1,11 @@
 import { parseLanguageTag } from "../LanguageTag";
 
 describe("parseLanguageTag", () => {
+  test("two-letter primary language", () => {
+    const languageTag = parseLanguageTag("jp");
+    expect(languageTag?.language).toMatch("jp");
+  });
+
   test("three-letter primary language", () => {
     const cases = ["mas", "ast"];
     for (const tag of cases) {
@@ -9,43 +14,55 @@ describe("parseLanguageTag", () => {
     }
   });
 
+  test("truncated primary language", () => {
+    expect(parseLanguageTag("b")).toBeNull();
+  });
+
+  test("primary language is too long", () => {
+    expect(parseLanguageTag("generation")).toBeNull();
+  });
+
   test("extended language", () => {
     const cases = ["ar-afb", "zh-yue", "zh-min-nan"];
     const parts = [
-      { language: "ar", extendedLanguages: ["afb"] },
-      { language: "zh", extendedLanguages: ["yue"] },
-      { language: "zh", extendedLanguages: ["min", "nan"] },
+      { extendedLanguages: ["afb"] },
+      { extendedLanguages: ["yue"] },
+      { extendedLanguages: ["min", "nan"] },
     ];
     for (let i = 0; i < cases.length; i++) {
       const languageTag = parseLanguageTag(cases[i]);
       const expected = parts[i];
-      expect(languageTag?.language).toMatch(expected.language);
-      expect(languageTag?.extendedLanguages?.[0]).toMatch(
-        expected.extendedLanguages[0]
-      );
+      for (let j = 0; j < expected.extendedLanguages.length; j++) {
+        expect(languageTag?.extendedLanguages?.[j]).toMatch(
+          expected.extendedLanguages[j]
+        );
+      }
     }
   });
 
   test("region", () => {
     const cases = ["en-GB", "es-005", "es-419"];
-    const parts = [
-      { language: "en", region: "GB" },
-      { language: "es", region: "005" },
-      { language: "es", region: "419" },
-    ];
+    const expected = ["GB", "005", "419"];
     for (let i = 0; i < cases.length; i++) {
       const languageTag = parseLanguageTag(cases[i]);
-      const expected = parts[i];
-      expect(languageTag?.language).toMatch(expected.language);
-      expect(languageTag?.region).toMatch(expected.region);
+      expect(languageTag?.region).toMatch(expected[i]);
     }
   });
 
+  test("region shouldn't mix alphabetic and numeric characters", () => {
+    expect(parseLanguageTag("es-39B")).toBeNull();
+  });
+
   test("extension", () => {
-    const cases = ["ar-u-nu-latn", "gsw-u-sd-chzh", "de-DE-u-co-phonebk"];
+    const cases = [
+      "ar-u-nu-latn",
+      "gsw-u-sd-chzh",
+      "de-DE-u-co-phonebk",
+      "he-IL-u-ca-hebrew-tz-jeruslm",
+      "zh-t-i0-pinyin",
+    ];
     const parts = [
       {
-        language: "ar",
         extensions: [
           {
             detail: "nu-latn",
@@ -54,7 +71,6 @@ describe("parseLanguageTag", () => {
         ],
       },
       {
-        language: "gsw",
         extensions: [
           {
             detail: "sd-chzh",
@@ -63,7 +79,6 @@ describe("parseLanguageTag", () => {
         ],
       },
       {
-        language: "de",
         extensions: [
           {
             detail: "co-phonebk",
@@ -71,11 +86,26 @@ describe("parseLanguageTag", () => {
           },
         ],
       },
+      {
+        extensions: [
+          {
+            detail: "ca-hebrew-tz-jeruslm",
+            singleton: "u",
+          },
+        ],
+      },
+      {
+        extensions: [
+          {
+            detail: "i0-pinyin",
+            singleton: "t",
+          },
+        ],
+      },
     ];
     for (let i = 0; i < cases.length; i++) {
       const languageTag = parseLanguageTag(cases[i]);
       const expected = parts[i];
-      expect(languageTag?.language).toMatch(expected.language);
       expect(languageTag?.extensions?.[0].detail).toMatch(
         expected.extensions[0].detail
       );
@@ -87,19 +117,36 @@ describe("parseLanguageTag", () => {
 
   test("private use", () => {
     const languageTag = parseLanguageTag("ttt-x-ismai");
-    expect(languageTag?.language).toMatch("ttt");
     expect(languageTag?.privateUse).toMatch("ismai");
   });
 
+  test("private use subtag may contain hyphens", () => {
+    const languageTag = parseLanguageTag(
+      "zh-yue-Latn-CN-pinyin-a-extend1-x-foobar-private1"
+    );
+    expect(languageTag?.privateUse).toMatch("foobar-private1");
+  });
+
   test("script", () => {
-    const cases = ["zh-Hant-HK"];
-    const parts = [{ language: "zh", region: "HK", script: "Hant" }];
+    const cases = [
+      "zh-Hant-HK",
+      "zh-yue-Latn-CN-pinyin-a-extend1-x-foobar-private1",
+      "sr-Cyrl-CS",
+    ];
+    const expected = ["Hant", "Latn", "Cyrl"];
     for (let i = 0; i < cases.length; i++) {
       const languageTag = parseLanguageTag(cases[i]);
-      const expected = parts[i];
-      expect(languageTag?.language).toMatch(expected.language);
-      expect(languageTag?.region).toMatch(expected.region);
-      expect(languageTag?.script).toMatch(expected.script);
+      expect(languageTag?.script).toMatch(expected[i]);
     }
+  });
+
+  test("variant", () => {
+    const languageTag = parseLanguageTag("nn-hognorsk");
+    expect(languageTag?.variants?.[0]).toMatch("hognorsk");
+  });
+
+  test("variant may contain a date", () => {
+    const languageTag = parseLanguageTag("pt-BR-colb1945");
+    expect(languageTag?.variants?.[0]).toMatch("colb1945");
   });
 });
